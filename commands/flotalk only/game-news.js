a4e2da236a73b9c1b91d6696487c5ae337fd64a2
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-17 13:25:50 +0100

    yo

---
+++ commands/flotalk only/game-news.js
@@ -0,0 +1,11 @@
+module.exports = {
+	testOnly: true,
+	callback: ({ message }) => {
+		const Discord = require('discord.js');
+		const embed = new Discord.MessageEmbed()
+			.setColor('#f77600')
+			.setTitle('Game-News:')
+			.setDescription('**Aktuell werden von diesen Games News in #game-news gepostet:**\n- Bloons Tower Defense (Tweets von @ninjakiwigames)\n- Borderlands (SHiFT Codes)\n- Destiny 2 (Weekly Reset & Xur Übersichten, Tweets von @BungieHelp)\n- Final Fantasy XIV (Lodestone Updates und Fashion Report)\n- Minecraft (Neue Updates für Java- & Bedrock Edtion\n- Raft (Neue Updates)');
+		return message.channel.send(embed);
+	},
+};
