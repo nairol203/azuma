047e32e9d1aa721cb47434d05ebd503c4ea93ce2
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-18 23:19:48 +0100

    Update triggered.js

--- commands/fun/triggered.js
+++ commands/fun/triggered.js
@@ -3,8 +3,10 @@ const canvacord = require('canvacord');
 
 module.exports = {
 	callback: async ({ message }) => {
+		const target = message.mentions.users.first() || message.author;
+
 		message.delete();
-		const avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
+		const avatar = target.displayAvatarURL({ dynamic: false, format: 'png' });
 		const image = await canvacord.Canvas.trigger(avatar);
 		const attachment = new Discord.MessageAttachment(image, 'triggered.gif');
 		return message.channel.send(attachment);
