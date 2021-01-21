17b4a581f5375c8923e7dffb5022b1e09ed6111d
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-21 23:56:41 +0100

    Update info.js

--- commands/allgemein/info.js
+++ commands/allgemein/info.js
@@ -10,7 +10,7 @@ module.exports = {
 			.setAuthor(`Information über ${client.user.username}`, client.user.displayAvatarURL())
 			.addFields (
 				{ name: 'Allgemeine Info', value: 'Dieser Bot wurde von <@!255739211112513536> programmiert. Falls irgendetwas nicht funktioniert, schreibt ihm!' },
-				{ name: 'Bekannte Bugs', value: `\`${prefix}fish\` zieht keine Coins bei einem Angelversuch ab\n- \`${prefix}help <arg>\` funktioniert nicht` },
+				{ name: 'Bekannte Bugs', value: `- \`${prefix}fish\` zieht keine Coins bei einem Angelversuch ab\n- \`${prefix}help <arg>\` funktioniert nicht` },
 				{ name: 'Roadmap', value: '- Warn Command\n- Spotify Integration\n- Und mehr!' },
 				{ name: 'Version', value: version, inline: true },
 				{ name: 'Aktuelle Laufzeit', value: `${hours} h (${minutes} min)`, inline: true },
