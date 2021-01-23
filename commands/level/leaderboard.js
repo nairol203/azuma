e6b68946b9ed124333c3dd657dd164e9441eb659
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-23 20:59:32 +0100

    Create leaderboard.js

---
+++ commands/level/leaderboard.js
@@ -0,0 +1,22 @@
+const Levels = require('discord-xp');
+const Discord = require('discord.js');
+
+module.exports = {
+	aliases: 'lb',
+	callback: async ({ client, message }) => {
+		const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10);
+
+		if (rawLeaderboard.length < 1) return message.reply('Aktuell ist hat noch niemand XP gesammelt.');
+
+		const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true);
+
+		const lb = leaderboard.map(e => `\`${e.position}.\` **${e.username}#${e.discriminator}:**\nLevel: ${e.level} • XP: ${e.xp.toLocaleString()}`);
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle(`Leaderboard von ${message.guild.name}`)
+			.setDescription(`${lb.join('\n\n')}`)
+			.setColor('f77600')
+			.setThumbnail(`${message.guild.iconURL()}`);
+		message.channel.send(embed);
+	},
+};
