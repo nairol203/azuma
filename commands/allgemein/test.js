1bb228380f5e29524c347411905485f88dbfbfdf
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-23 20:59:34 +0100

    Update test.js

--- commands/allgemein/test.js
+++ commands/allgemein/test.js
@@ -2,8 +2,9 @@ const cooldowns = new Set();
 
 module.exports = {
 	callback: ({ message }) => {
-		if (cooldowns.has(message.author.id)) return message.channel.send(`SPAMMER DETECTED: ${cooldowns.has(message.author.id)}`);
+		if (cooldowns.has(message.author.id)) return message.channel.send(`SPAMMER: ${cooldowns.has(message.author.id)}`);
 		cooldowns.add(message.author.id);
 		setTimeout(() => cooldowns.delete(message.author.id), 15000);
+		message.channel.send('SPAMMER: false');
 	},
 };
