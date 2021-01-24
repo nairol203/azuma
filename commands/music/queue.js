5be8fe130c650fe0fb9f5c777bf7eaa2e4878e6f
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-24 21:32:07 +0100

    altes Musik Modul

--- commands/music/queue.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	callback: ({ client, message }) => {
-		const queue = client.distube.getQueue(message);
-		message.channel.send('Song Queue:\n' + queue.songs.map((song, id) =>
-			`**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``,
-		).slice(0, 10).join('\n'));
-	},
-};
