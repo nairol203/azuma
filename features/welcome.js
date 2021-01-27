3bee74a1ecd306d0dbe3889c10dd83ab5c1e859f
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-27 15:55:22 +0100

    Update welcome.js

--- features/welcome.js
+++ features/welcome.js
@@ -1,6 +1,7 @@
 module.exports = (client) => {
+	const autologs = client.channels.cache.find(channel => channel.id === '781501076725563413');
+
 	client.on('guildMemberAdd', member => {
-		if(member.bot) return;
 		if (member.guild.id === '255741114273759232') {
 			member.roles.add('770782137338036265');
 			member.send(`Wilkommen auf **${member.guild.name}**! Falls du Hilfe brauchst schau in <#786936121774702603> <:peepoHug:750428178979225640>`);
@@ -9,5 +10,10 @@ module.exports = (client) => {
 		else if (member.guild.id === '506518638564737025') {
 			member.roles.add('795453805789446144');
 		}
+		autologs.send(`📥 ${member.user} ist dem Server beigetreten.`);
+	});
+
+	client.on('guildMemberRemove', member => {
+		autologs.send(`📤 ${member.user} hat den Server verlassen.`);
 	});
 };
