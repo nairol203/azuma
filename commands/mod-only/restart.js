254bb401f69622ae7ea16c11b3c19f227c7ffa81
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-08 19:41:34 +0100

    Delete restart.js

--- commands/mod-only/restart.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	ownerOnly: true,
-	aliases: 'kill',
-	callback: ({ client, message }) => {
-		message.channel.send('Restarting...');
-		client.destroy();
-		client.login(process.env.TOKEN);
-	},
-};
