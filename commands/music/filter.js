6ecafdb276cd942153bf10bfa7e92129ff434fb9
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-10 10:46:38 +0100

    Update filter.js

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -1,11 +1,13 @@
 module.exports = {
 	minArgs: 1,
 	maxArgs: 1,
-	expectedArgs: '<3d | bassboost | echo | karaoke | nightcore | vaporwave | flanger | gate | haas | reverse | surround | mcompand | phaser | tremolo | earwax>',
+	expectedArgs: '`<filter>',
 	callback: ({ client, message, args }) => {
 		if (!message.member.voice.channel) return message.channel.send('<:no:767394810909949983> Du musst in einem Sprachkanal sein um diesen Command zu benutzen!');
 		const queue = client.distube.getQueue(message);
 		if (!queue) return message.channel.send('<:no:767394810909949983> | Du diesen Befehl nur benutzen wenn Musik gespielt wird.');
+
+		if(args[0] === 'help') return message.channel.send('Alle Filter:\n- 3d\n- bassboost\n- echo\n- karaoke\n- nightcore\n- vaporwave\n- flanger\n- gate\n- haas\n- reverse\n- surround\n- mcompand\n- phaser\n- tremolo\n- earwax');
 
 		if (args[0] === 'off' && queue.filter) client.distube.setFilter(message, queue.filter);
 		else if (Object.keys(client.distube.filters).includes(args[0])) client.distube.setFilter(message, args[0]);
