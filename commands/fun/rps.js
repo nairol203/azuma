361ade8312692153ac51eb2f1f4fe355eb1fd502
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-13 16:31:28 +0100

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -2,6 +2,100 @@ const economy = require('../../features/economy');
 
 module.exports = {
 	callback: async ({ message, args }) => {
+		const target = message.mentions.users.first();
+		const test = message.author;
+		if (target) {
+			if (!args[1]) return message.channel.send('Lass uns doch um etwas Geld spielen! `!rps <user> <coins>`');
+			if (target.id === test.id) return message.channel.send('Du kannst doch nicht mit dir selbst spielen <:FeelsDankMan:780215649384398908>');
+
+			if ((isNaN(args[1])) || args[1] < 1) return message.channel.send('Bitte setze einen gültigen Betrag!');
+			const coinsOwned = await economy.getCoins(message.author.id);
+			if (coinsOwned < args[1]) return message.channel.send(`Du hast doch gar keine ${args[1]} Coins <:Susge:809947745342980106>`);
+
+			const filter = m => m.author.id === target.id;
+			message.channel.send(`${target}, du wurdest zu einer Runde Schere, Stein, Papier von ${message.author} mit ${args[1]} Coins Einsatz herausgefordert!\nSchreibe \`accept\` in den Chat um die Herausforderung anzunehmen!`).then(() => {
+				message.channel.awaitMessages(filter, {
+					max: 1,
+					time: 15000,
+					errors: ['time'],
+				})
+					.then(async message => {
+						message = message.first();
+						if (message.content.toLowerCase() == 'accept') {
+							const targetCoins = await economy.getCoins(target.id);
+							if (targetCoins < args[1]) return message.channel.send(`Du kannst nicht teilnehmen da du keine ${args[1]} Coins hast <:Susge:809947745342980106>`);
+
+							message.channel.send('Ich werde euch nacheinander per DM fragen, was ihr nimmt! Anwortet bitte jeweils innerhalb von 15 Sekunden!');
+
+							const msg = await test.send('Nimmst du Schere, Stein oder Papier?');
+							const filter1 = collected => collected.author.id === test.id;
+							const collected = await msg.channel.awaitMessages(filter1, {
+								max: 1,
+								time: 15000,
+							}).catch(() => {
+								message.test.send('Du warst zu langsam!');
+							});
+
+							const msg1 = await message.author.send('Nimmst du Schere, Stein oder Papier?');
+							const filter2 = collected1 => collected1.author.id === target.id;
+							const collected1 = await msg1.channel.awaitMessages(filter2, {
+								max: 1,
+								time: 15000,
+							}).catch(() => {
+								message.author.send('Du warst zu langsam!');
+							});
+
+							if(collected.first().content.toLowerCase() === 'stein') {
+								if(collected1.first().content.toLowerCase() === 'stein') return message.channel.send('Ihr habt beide Stein genommen! Unendschieden!');
+								if(collected1.first().content.toLowerCase() === 'schere') {
+									await economy.addCoins(test.id, args[1]);
+									await economy.addCoins(target.id, args[1] * -1);
+									message.channel.send(`${test} hat Stein genommen und ${target} Schere!\nDamit gewinnt ${test}!`);
+								}
+								if(collected1.first().content.toLowerCase() === 'papier') {
+									await economy.addCoins(test.id, args[1] * -1);
+									await economy.addCoins(target.id, args[1]);
+									message.channel.send(`${test} hat Stein genommen und ${target} Papier!\nDamit gewinnt ${target}!`);
+								}
+							}
+							if(collected.first().content.toLowerCase() === 'schere') {
+								if(collected1.first().content.toLowerCase() === 'schere') return message.channel.send('Ihr habt beide Schere genommen! Unendschieden!');
+								if(collected1.first().content.toLowerCase() === 'papier') {
+									await economy.addCoins(test.id, args[1]);
+									await economy.addCoins(target.id, args[1] * -1);
+									message.channel.send(`${test} hat Schere genommen und ${target} Papier!\nDamit gewinnt ${test}!`);
+								}
+								if(collected1.first().content.toLowerCase() === 'stein') {
+									await economy.addCoins(test.id, args[1] * -1);
+									await economy.addCoins(target.id, args[1]);
+									message.channel.send(`${test} hat Schere genommen und ${target} Stein!\nDamit gewinnt ${target}!`);
+								}
+							}
+							if(collected.first().content.toLowerCase() === 'papier') {
+								if(collected1.first().content.toLowerCase() === 'papier') return message.channel.send('Ihr habt beide Papier genommen! Unendschieden!');
+								if(collected1.first().content.toLowerCase() === 'stein') {
+									await economy.addCoins(test.id, args[1]);
+									await economy.addCoins(target.id, args[1] * -1);
+									message.channel.send(`${test} hat Papier genommen und ${target} Stein!\nDamit gewinnt ${test}!`);
+								}
+								if(collected1.first().content.toLowerCase() === 'schere') {
+									await economy.addCoins(test.id, args[1] * -1);
+									await economy.addCoins(target.id, args[1]);
+									message.channel.send(`${test} hat Papier genommen und ${target} Schere!\nDamit gewinnt ${target}!`);
+								}
+							}
+						}
+						else {
+							message.channel.send('Du hast nicht `accept` geschrieben!');
+						}
+					})
+					.catch(collected => {
+						message.channel.send('Timeout! Bitte anwortet immer innerhalb von 15 Sekunden!');
+					});
+			});
+			return;
+		}
+
 		if (!args.length) return message.channel.send('Lass uns doch um etwas Geld spielen! `!rps <coins>`');
 
 		if ((isNaN(args[0])) || args[0] < 1) return message.channel.send('Bitte setze einen gültigen Betrag!');
