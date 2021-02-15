1303031f8b2a64942386a0ab4cb0efa54d3954c8
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-15 23:45:01 +0100

    Update voice.js

--- commands/allgemein/voice.js
+++ commands/allgemein/voice.js
@@ -74,7 +74,7 @@ module.exports = {
 			const mention = client.users.cache.find(u => u.username == `${name}`);
 			if (mention === undefined) return channel.send('Ich konnte niemand finden, der so heißt. Tipp: Ich kann aktuell noch keine Nicknamen verstehen!');
 			voiceChannel.updateOverwrite(mention.id, { CONNECT: true });
-			channel.send(`Ich habe <@${mention.id}> den Schlüssel zu deinem Zimmer gegeben.`);
+			channel.send(`Ich habe ${mention} den Schlüssel zu deinem Zimmer gegeben.`);
 		}
 		else if (args[0] === 'limit') {
 			const amount = parseInt(args[1]) + 1;
@@ -82,7 +82,7 @@ module.exports = {
 			if (amount <= 0) return;
 			const channelLimit = args[1];
 			voiceChannel.setUserLimit(channelLimit);
-			channel.send(`Alles klar, ich habe das Personenlimit auf ${channelLimit} gesetzt.`);
+			channel.send(`Alles klar, ich habe das Personenlimit auf \`${channelLimit}\` gesetzt.`);
 			const uploadChannelLimit = await customs.findOneAndUpdate(
 				{
 					userId,
