aadd54223048f1f0482b8e11e7e73ed4abe736db
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-15 11:54:21 +0100

    Update customs.js

--- features/customs.js
+++ features/customs.js
@@ -67,8 +67,8 @@ module.exports = client => {
 					{ name: 'Abschließen:', value: '`!voice lock`', inline: true },
 					{ name: 'Aufschließen:', value: '`!voice unlock`', inline: true },
 					{ name: 'Das Türschild ändern:', value: '`!voice name <name>`', inline: true },
-					{ name: 'Personen einen Zweitschlüssel geben:', value: '`!text reject <@user>`', inline: false },
-					{ name: 'Personen den Zweitschlüssel wegnehmen:', value: '`!text permit <@user>`', inline: false },
+					{ name: 'Personen einen Zweitschlüssel geben:', value: '`!text permit <@user>`', inline: false },
+					{ name: 'Personen den Zweitschlüssel wegnehmen:', value: '`!text reject <@user>`', inline: false },
 					{ name: 'Ein Personenlimit einstellen:', value: '`!voice limit <number>`', inline: false },
 				)
 				.setColor('#b8ff00')
