97ed2570e0c8befa37324d761161ee94e1ab9969
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-15 07:56:13 +0100

    Update voice.js

--- commands/allgemein/voice.js
+++ commands/allgemein/voice.js
@@ -2,7 +2,7 @@ const customsMain = require('../schemas/customs-main');
 
 module.exports = {
 	callback: async ({ message, args }) => {
-		const { guild } = message;
+		const { author, channel, guild } = message;
 
 		if(args[0] === 'setup') {
 			const customChannel = await guild.channels.create('Customs', {
@@ -17,19 +17,32 @@ module.exports = {
 			return;
 		}
 		else if (args[0] === 'lock') {
-			message.reply('coming soon');
+			channel.updateOverwrite(author, { VIEW_CHANNEL: true });
+			channel.updateOverwrite('255741114273759232', { VIEW_CHANNEL: false });
+			channel.send('Nur noch die User, denen du Rechte gegeben hast, können auf diesen Kanal zugreifen.');
 		}
 		else if (args[0] === 'unlock') {
-			message.reply('coming soon');
+			channel.updateOverwrite('255741114273759232', { VIEW_CHANNEL: true });
+			channel.send('Alle User können jetzt auf diesen Kanal zugreifen.');
 		}
 		else if (args[0] === 'name') {
-			message.reply('coming soon');
+			if(!args[1]) return message.reply('versuche es so: `!voice name <name>`');
+			channel.setName(args[1]);
+			channel.send(`Der Name des Kanals wurde geändert. Neuer Name: \`${args[1]}\`.`);
 		}
 		else if (args[0] === 'reject') {
-			message.reply('coming soon');
+			if(!args[1]) return message.reply('versuche es so: `!voice reject <@user>`');
+			const mention = args[1].toString().replace('<@!', '');
+			const mentionId = mention.toString().replace('>', '');
+			channel.updateOverwrite(mentionId, { VIEW_CHANNEL: false });
+			channel.send(`Du hast <@${mentionId}> den Zugriff auf diesen Kanal verweigert.`);
 		}
 		else if (args[0] === 'permit') {
-			message.reply('coming soon');
+			if(!args[1]) return message.reply('versuche es so: `!voice permit <@user>');
+			const mention = args[1].toString().replace('<@!', '');
+			const mentionId = mention.toString().replace('>', '');
+			channel.updateOverwrite(mentionId, { VIEW_CHANNEL: true });
+			channel.send(`Du hast <@${mentionId}> Zugriff auf diesen Kanal gegeben.`);
 		}
 		else if (args[0] === 'limit') {
 			message.reply('coming soon');
