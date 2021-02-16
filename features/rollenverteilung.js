dd120f1a4d791a950c4da0f072391e7a6d6ce214
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-16 14:34:22 +0100

    Update rollenverteilung.js

--- features/rollenverteilung.js
+++ features/rollenverteilung.js
@@ -14,7 +14,9 @@ const firstMessage = async (client, id, text, reactions = []) => {
 		const embed = new Discord.MessageEmbed()
 			.setTitle(':clipboard: Rollenverteilung')
 			.setDescription('```Füge eine Reaktion hinzu oder entferne eine, je nach dem welche Rolle du haben willst (●\'◡\'●)```\n<:EpicGames:782650903156621382> **Free Epic Games** - Bekomme immer eine Benachrichtigung wenn ein Spiel im Epic Store gratis ist!\n\n:bell: **Server-News** - Bekomme immer eine Benachrichtigung wenn ein Update von diesem Server gepostet wird!')
-			.setColor('#f77600');
+			.setColor('#f77600')
+			.setFooter('Letztes Update')
+			.setTimestamp('2021-02-16T13:31:00.000Z');
 		if (messages.size === 0) {
 			channel.send(embed).then((message) => {
 				addReactions(message, reactions);
