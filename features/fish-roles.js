965260d451877b9b71ae81141ef5bfdd1227e48b
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-17 13:20:44 +0100

    Update fish-roles.js

--- features/fish-roles.js
+++ features/fish-roles.js
@@ -8,6 +8,7 @@ module.exports = (client) => {
 		const uncommon = await fishing.getUncommon(userId); const rare = await fishing.getRare(userId);
 
 		const anfänger = message.guild.roles.cache.find(role => role.name === 'Angel-Anfänger');
+		if (anfänger === undefined) return;
 		if (rare < 1 & uncommon >= 25 & uncommon < 125) {
 			message.guild.members.cache.get(message.author.id).roles.add(anfänger);
 		}
@@ -15,6 +16,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(anfänger);
 		}
 		const bronze = message.guild.roles.cache.find(role => role.name === 'Bronze-Angler');
+		if (bronze === undefined) return;
 		if (rare >= 5 & rare < 10) {
 			message.guild.members.cache.get(message.author.id).roles.add(bronze);
 		}
@@ -22,6 +24,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(bronze);
 		}
 		const silber = message.guild.roles.cache.find(role => role.name === 'Silber-Angler');
+		if (silber === undefined) return;
 		if (rare >= 10 & rare < 15) {
 			message.guild.members.cache.get(message.author.id).roles.add(silber);
 		}
@@ -29,6 +32,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(silber);
 		}
 		const gold = message.guild.roles.cache.find(role => role.name === 'Gold-Angler');
+		if (gold === undefined) return;
 		if (rare >= 15 & rare < 20) {
 			message.guild.members.cache.get(message.author.id).roles.add(gold);
 		}
@@ -36,6 +40,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(gold);
 		}
 		const platin = message.guild.roles.cache.find(role => role.name === 'Platin-Angler');
+		if (platin === undefined) return;
 		if (rare >= 20 & rare < 30) {
 			message.guild.members.cache.get(message.author.id).roles.add(platin);
 		}
@@ -43,6 +48,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(platin);
 		}
 		const elite = message.guild.roles.cache.find(role => role.name === 'Elite-Fischer');
+		if (elite === undefined) return;
 		if (rare >= 30 & rare < 40) {
 			message.guild.members.cache.get(message.author.id).roles.add(elite);
 		}
@@ -50,6 +56,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(elite);
 		}
 		const meister = message.guild.roles.cache.find(role => role.name === 'Angelmeister');
+		if (meister === undefined) return;
 		if (rare >= 40 & rare < 50) {
 			message.guild.members.cache.get(message.author.id).roles.add(meister);
 		}
@@ -57,6 +64,7 @@ module.exports = (client) => {
 			message.guild.members.cache.get(message.author.id).roles.remove(meister);
 		}
 		const wahrerMeister = message.guild.roles.cache.find(role => role.name === 'Wahrer Angelmeister');
+		if (wahrerMeister === undefined) return;
 		if (rare >= 50) {
 			message.guild.members.cache.get(message.author.id).roles.add(wahrerMeister);
 		}
