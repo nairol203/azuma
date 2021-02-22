630b1dc424e10676c31ade3eb555f506dbe15e30
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-22 01:07:19 +0100

    Update coinflip.js

--- commands/games/coinflip.js
+++ commands/games/coinflip.js
@@ -6,13 +6,13 @@ module.exports = {
 		const test = message.author;
 		const guildId = message.guild.id;
 
-		if (!target) return message.reply('`!coinflip <@user> <coins>`');
-		if (!args[1]) return message.reply('du musst schon etwas Geld setzen! -> `!coinflip <@user> <coins>`');
+		if (!target) return message.reply('`!coinflip <@user> <credits>`');
+		if (!args[1]) return message.reply('du musst schon etwas Geld setzen! -> `!coinflip <@user> <credits>`');
 		if (target.id === test.id) return message.channel.send('Du kannst doch nicht mit dir selbst spielen <:FeelsDankMan:780215649384398908>');
 
 		if ((isNaN(args[1])) || args[1] < 1) return message.channel.send('Bitte setze einen gültigen Betrag!');
 		const coinsOwned = await economy.getCoins(guildId, message.author.id);
-		if (coinsOwned < args[1]) return message.channel.send(`Du hast doch gar keine ${args[1]} Coins <:Susge:809947745342980106>`);
+		if (coinsOwned < args[1]) return message.channel.send(`Du hast doch gar keine ${args[1]} 💵!`);
 
 		const messages = ['zahl', 'kopf'];
 		const randomMessage = messages[Math.floor(Math.random() * messages.length)];
@@ -28,18 +28,18 @@ module.exports = {
 					message = message.first();
 					if (message.content.toLowerCase() == 'accept') {
 						const targetCoins = await economy.getCoins(guildId, target.id);
-						if (targetCoins < args[1]) return message.channel.send(`Du kannst nicht teilnehmen da du keine ${args[1]} Coins hast <:Susge:809947745342980106>`);
+						if (targetCoins < args[1]) return message.channel.send(`Du kannst nicht teilnehmen da du keine ${args[1]} 💵 hast.`);
 
 						message.channel.send('<a:Coin:795346652599812147>*flipping...*');
 
 						setTimeout(async function() {
 							if(randomMessage === 'zahl') {
-								message.channel.send(`${target} hat ${args[1]} Coins gewonnen!`);
+								message.channel.send(`${target} hat ${args[1]} 💵 gewonnen!`);
 								await economy.addCoins(guildId, target.id, args[1]);
 								await economy.addCoins(guildId, test.id, args[1] * -1);
 							}
 							if(randomMessage === 'kopf') {
-								message.channel.send(`${test} hat ${args[1]} Coins gewonnen!`);
+								message.channel.send(`${test} hat ${args[1]} 💵 gewonnen!`);
 								await economy.addCoins(guildId, target.id, args[1] * -1);
 								await economy.addCoins(guildId, test.id, args[1]);
 							}
