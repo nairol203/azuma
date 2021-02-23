dd07864d4da2a618577c79cd86fedfb95b4c3fea
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-23 08:59:47 +0100

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -4,23 +4,18 @@ const economy = require('../../features/economy');
 const business = require('../../features/business');
 
 module.exports = {
-	cooldown: '8h',
+	// cooldown: '8h',
 	callback: async ({ message }) => {
 		const { author, guild, channel } = message;
 		const guildId = guild.id;
 		const userId = author.id;
-
+		const getBusiness = await business.getBusiness(guildId, userId);
+		if (getBusiness === null) return channel.send('<:no:767394810909949983> | Du hast kein Unternehmen, kaufe eins mit `!business buy`!');
 		const company = await business.setCompany(guildId, userId);
 		const profit = await business.checkProfit(guildId, userId);
-		const getBusiness = await business.getBusiness(guildId, userId);
-
-		if (getBusiness === null) return channel.send('<:no:767394810909949983> | Du hast kein Unternehmen, kaufe eins mit `!business buy`!');
-
 		const d = Math.random();
-
 		if (d > 0.15) {
 			await economy.addCoins(guildId, userId, profit);
-
 			const embed = new Discord.MessageEmbed()
 				.setTitle('Verkauf erfolgreich')
 				.setDescription(`Du hast die hergestellte Ware von deiner ${company.name} verkauft.`)
