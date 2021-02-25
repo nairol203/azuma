655d385f1806747e91719592b0df6cb412ddc252
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-25 23:40:04 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,32 +1,25 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	category: 'Help',
-	description: 'Alle Commands von diesem Bot.',
 	expectedArgs: '<command>',
 	minArgs: 0,
 	maxArgs: 1,
-	callback: ({ message, args, instance }) => {
+	callback: ({ message, instance }) => {
 		const prefix = instance.getPrefix(message.guild);
 
-		if (!args.length) {
-			const embed = new Discord.MessageEmbed()
-				.setColor('#f77600')
-				.setTitle(`Befehle von ${message.guild.name}`)
-				.setDescription(`Tipp: Benutze \`${prefix}help <command>\` um mehr über einen bestimmten Befehl zu erfahren.`)
-				.setThumbnail(`${message.guild.iconURL()}`)
-				.addFields (
-					{ name: 'Misc', value: '`howto` `info` `ping` `server` `text`', inline: true },
-					{ name: 'Economy', value: '`credits` `daily` `pay` `business` `work`', inline: true },
-					{ name: 'Games', value: '`coinflip` `fish` `rps`', inline: true },
-					{ name: 'Level', value: '`rank` `leaderboard`', inline: true },
-					{ name: 'Musik', value: '`loop` `nowplaying` `pause` `play` `queue` `resume` `search` `skip` `stop` `volume`', inline: true },
-					{ name: 'Admin', value: '`ban` `deploy` `ismuted` `kick` `mute` `prune` `unban` `unmute`', inline: true },
-				);
-			return message.channel.send(embed);
-		}
-		else {
-			message.channel.send('<:no:767394810909949983> | Error occured while running help command.');
-		}
+		const embed = new Discord.MessageEmbed()
+			.setColor('#f77600')
+			.setTitle(`Befehle von ${message.guild.name}`)
+			.setDescription(`Tipp: Benutze \`${prefix}help <command>\` um mehr über einen bestimmten Befehl zu erfahren.`)
+			.setThumbnail(`${message.guild.iconURL()}`)
+			.addFields (
+				{ name: 'Misc', value: '`howto` `info` `ping` `server` `text`', inline: true },
+				{ name: 'Economy', value: '`credits` `daily` `pay` `business` `work`', inline: true },
+				{ name: 'Games', value: '`coinflip` `fish` `rps`', inline: true },
+				{ name: 'Level', value: '`rank` `leaderboard`', inline: true },
+				{ name: 'Musik', value: '`loop` `nowplaying` `pause` `play` `queue` `resume` `search` `skip` `stop` `volume`', inline: true },
+				{ name: 'Admin', value: '`ban` `deploy` `ismuted` `kick` `mute` `prune` `unban` `unmute`', inline: true },
+			);
+		return message.channel.send(embed);
 	},
 };
