8e86765584d0429ce109b0030f070f25a719d2bd
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-25 22:09:41 +0100

    Delete setstatus.js

--- commands/mod-only/setstatus.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	ownerOnly: true,
-	callback: ({ client, args }) => {
-		const argument = args.join(' ');
-		const arg = argument.toString().replace(args[1], '');
-		client.user.setActivity(arg, { type: args[1] });
-	},
-};
