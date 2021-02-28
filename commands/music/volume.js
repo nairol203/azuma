30b50cde4033e4b98fc5bfabe3cd167d78da2de0
nairol <73656754+nairol203@users.noreply.github.com>
2021-03-01 00:17:26 +0100

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -8,12 +8,15 @@ module.exports = {
 	callback: ({ message, args }) => {
 		const serverQueue = music.serverQueue(message);
 
+		if(!args.length) return message.channel.send(`Die Lautstärke des Bot's ist **${serverQueue.volume}**.`);
+		if(isNaN(args[0])) return message.channel.send('<:no:767394810909949983> | Keine gültige Eingabe erkannt.');
+		const amount = parseInt(args[0]);
+		if (amount > 5) return message.channel.send('Wenn du dir die Ohren wegschallern willst hör lieber Metal!');
+
 		if(!message.member.voice.channel) return message.channel.send('<:no:767394810909949983> | Du musst in einem Sprachkanal sein um diesen Command zu benutzen!');
 		if(!serverQueue) return message.channel.send('<:no:767394810909949983> | Es wird gerade nichts gespielt');
-		if(!args[0]) return message.channel.send(`Die Lautstärke des Bot's ist **${serverQueue.volume}**.`);
-		if(isNaN(args[0])) return message.channel.send('<:no:767394810909949983> | Keine gültige Eingabe erkannt.');
-		serverQueue.volume = args[0];
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-		message.channel.send(`Die Lautstärke wurde zu **${args[0]}** geändert.`);
+		serverQueue.volume = amount;
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(amount / 5);
+		message.channel.send(`Die Lautstärke wurde zu **${amount}** geändert.`);
 	},
 };
