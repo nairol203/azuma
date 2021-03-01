899b8d60aba1a36bbdb7a58590aaf75b3207cb6f
nairol <73656754+nairol203@users.noreply.github.com>
2021-03-01 13:14:11 +0100

    Update prune.js

--- commands/mod-only/prune.js
+++ commands/mod-only/prune.js
@@ -4,19 +4,20 @@ module.exports = {
 	maxArgs: 1,
 	requiredPermissions: ['MANAGE_MESSAGES'],
 	callback: ({ message, args, instance }) => {
+		const { channel } = message;
 		const prefix = instance.getPrefix(message.guild);
-
-		if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
-
 		const amount = parseInt(args[0]) + 1;
-
 		if (amount <= 1 || amount > 100) {
-			return message.channel.send(`<:no:767394810909949983> Ungültiger Befehl, versuche es so: \`${prefix}prune <1-99>\``);
+			return channel.send(`<:no:767394810909949983> Ungültiger Befehl, versuche es so: \`${prefix}prune <1-99>\``);
 		}
-
-		message.channel.bulkDelete(amount, true).catch(error => {
-			console.error(error);
-			return message.channel.send('<:no:767394810909949983> Error occured while running prune command.');
-		});
+		channel.messages.fetch({ limit: amount })
+			.then(fetched => {
+				const notPinned = fetched.filter(fetchedMsg => !fetchedMsg.pinned);
+				channel.bulkDelete(notPinned, true);
+			})
+			.catch(error => {
+				console.log(error);
+				return channel.send('<:no:767394810909949983> Error occured while running prune command.');
+			});
 	},
 };
