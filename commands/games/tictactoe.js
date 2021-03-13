7a7703a9236e73acb626fb1f45d2463d7085b034
nairol <73656754+nairol203@users.noreply.github.com>
2021-03-13 14:56:40 +0100

    Create tictactoe.js

---
+++ commands/games/tictactoe.js
@@ -0,0 +1,8 @@
+module.exports = {
+    update: true,    
+    slash: true,
+    description: 'Spiele Tic Tac Toe!',
+    callback: ({}) => {
+        return 'Work in progress - coming soon!'
+    }
+}
