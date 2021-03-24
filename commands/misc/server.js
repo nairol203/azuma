ebc9be6c7b6e2408660de572bf4fa7ab203b8f9a
nairol <73656754+nairol203@users.noreply.github.com>
2021-03-24 12:37:54 +0100

    Update server.js

--- commands/misc/server.js
+++ commands/misc/server.js
@@ -7,17 +7,17 @@ module.exports = {
 		const guild = client.guilds.cache.get(interaction.guild_id);
 		const embed = new MessageEmbed()
 			.setColor('#f77600')
-			.setTitle(`${guild.name}`)
-			.setThumbnail(`${guild.iconURL()}`)
+			.setTitle(guild.name)
+			.setThumbnail(guild.iconURL())
 			.addFields (
-				{ name: 'Server ID:', value: `${guild.id}`, inline: true },
-				{ name: 'Owner:', value: `${guild.owner}`, inline: true },
-				{ name: 'Mitglieder:', value: `${guild.memberCount}` },
-				{ name: 'Rollen:', value: `${guild.roles.cache.size}`, inline: true },
-				{ name: 'Emojis:', value: `${guild.emojis.cache.size}`, inline: true },
-				{ name: 'Region:', value: `${guild.region}` },
+				{ name: 'Server ID:', value: guild.id, inline: true },
+				{ name: 'Owner:', value: guild.owner, inline: true },
+				{ name: 'Mitglieder:', value: guild.memberCount },
+				{ name: 'Rollen:', value: guild.roles.cache.size, inline: true },
+				{ name: 'Emojis:', value: guild.emojis.cache.size, inline: true },
+				{ name: 'Region:', value: guild.region },
 			)
-			.setFooter(`${guild.createdAt}`);
+			.setFooter(guild.createdAt);
 		return embed;
 	},
 };
