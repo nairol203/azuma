aa6015e004ef2e555f5a77b4ca13905bc3c6cb05
nairol <73656754+nairol203@users.noreply.github.com>
2021-04-15 16:32:32 +0200

    Update credits.js

--- commands/economy/credits.js
+++ commands/economy/credits.js
@@ -10,12 +10,9 @@ module.exports = {
 		}
 	],
 	callback: async ({ args, interaction }) => {
-		const user = interaction.member.user;
-		const userId = args.user || user.id;
+		const userId = args.user || interaction.member.user.id;
 		const guildId = interaction.guild_id;
-
 		const credits = await economy.getCoins(guildId, userId);
-
-		return `💵  |  **${user.username}**, du hast aktuell **${Intl.NumberFormat('de-DE', { maximumSignificantDigits: 3 }).format(credits)}** Credits.`
+		return `💵  |  **<@${userId}>**, du hast aktuell **${Intl.NumberFormat('de-DE', { maximumSignificantDigits: 10 }).format(credits)}** Credits.`
 	},
 };
