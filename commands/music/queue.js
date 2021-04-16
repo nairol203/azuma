d4e2f94fc04a8ff32d13aa7e1cf45a179c6e89ef
nairol <73656754+nairol203@users.noreply.github.com>
2021-04-17 01:06:04 +0200

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -6,12 +6,12 @@ module.exports = {
 	description: 'zeigt die aktuelle Song Queue an',
 	callback: ({ interaction }) => {
 		const sQ = serverQueue(interaction.guild_id);
-		if(!sQ) return  no + ' | Es wird gerade nichts gespielt.';
+		if(!sQ) return [ no + ' | Es wird gerade nichts gespielt.' ];
 		const embed = new MessageEmbed()
 			.setTitle('Queue')
 			.addFields(
 				{ name: 'Playing:', value: `[${sQ.songs[0].title}](${sQ.songs[0].url}) - \`${sQ.songs[0].duration}\`` },
-				{ name: 'Next songs:', value: sQ.songs.slice(1, 11).map(song => `**-** [${song.title}](${song.url}) - \`${song.duration}\``).join('\n') },
+				{ name: 'Next songs:', value: sQ.songs.slice(1, 11).map(song => `**-** [${song.title}](${song.url}) - \`${song.duration}\``).join('\n') || '/' },
 				{ name: 'Länge der Queue:', value: sQ.songs.length + ` song(s)`},
 			)
 			.setColor('#f77600');
