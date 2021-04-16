fdbe673d470011da40152f95f3cd11ee47edf490
nairol <73656754+nairol203@users.noreply.github.com>
2021-04-17 01:03:55 +0200

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -6,22 +6,15 @@ module.exports = {
 	description: 'zeigt die aktuelle Song Queue an',
 	callback: ({ interaction }) => {
 		const sQ = serverQueue(interaction.guild_id);
-
-		const embed = new MessageEmbed()
-		.setTitle('Queue')
-		.addFields(
-			{ name: 'Playing:', value: `[${sQ.songs[0].title}](${sQ.songs[0].url}) - \`${sQ.songs[0].duration}\`` },
-		)
-		.setColor('#f77600');
-
 		if(!sQ) return  no + ' | Es wird gerade nichts gespielt.';
-		if (sQ.songs.length > 10) {
-			embed.addField('Next song:', `**-** [${sQ.songs[1].title}](${sQ.songs[1].url}) - \`${sQ.songs[1].duration}\``)
-			embed.addField('Länge der Queue:', sQ.songs.length + ` song(s)`)
-			return embed
-		}
-		embed.addField('Next songs:', sQ.songs.map(song => `**-** [${song.title}](${song.url}) - \`${song.duration}\``).join('\n'))
-		embed.addField('Länge der Queue:', sQ.songs.length + ` song(s)`)
+		const embed = new MessageEmbed()
+			.setTitle('Queue')
+			.addFields(
+				{ name: 'Playing:', value: `[${sQ.songs[0].title}](${sQ.songs[0].url}) - \`${sQ.songs[0].duration}\`` },
+				{ name: 'Next songs:', value: sQ.songs.slice(1, 11).map(song => `**-** [${song.title}](${song.url}) - \`${song.duration}\``).join('\n') },
+				{ name: 'Länge der Queue:', value: sQ.songs.length + ` song(s)`},
+			)
+			.setColor('#f77600');
 		return embed;
 	},
 };
