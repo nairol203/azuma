1b4a76de84eb786280b472e8c7ba18f204cf50f1
nairol <73656754+nairol203@users.noreply.github.com>
2021-04-17 19:57:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -183,6 +183,9 @@ async function createApiMessage(interaction, content) {
 }
 
 client.on('message', async message => {
+	if (message.content.toLowerCase().startsWith('kek'.toLowerCase())) {
+		message.delete()
+	}
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
