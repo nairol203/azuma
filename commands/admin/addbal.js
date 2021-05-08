20bc75983c31debf9cb1a2925b8489f9757aea66
nairol <73656754+nairol203@users.noreply.github.com>
2021-05-08 14:47:19 +0200

    Create addbal.js

---
+++ commands/admin/addbal.js
@@ -0,0 +1,30 @@
+const economy = require('../../features/economy');
+
+module.exports = {
+  minArgs: 2,
+  maxArgs: 2,
+  expectedArgs: "<The target's @> <coin amount>",
+  callback: async (message, arguments) => {
+    const mention = message.mentions.users.first()
+
+    if (!mention) {
+      message.reply('Please tag a user to add coins to.')
+      return
+    }
+
+    const coins = arguments[1]
+    if (isNaN(coins)) {
+      message.reply('Please provide a valid numnber of coins.')
+      return
+    }
+
+    const guildId = message.guild.id
+    const userId = mention.id
+
+    const newCoins = await economy.addCoins(guildId, userId, coins)
+
+    message.reply(
+      `You have given <@${userId}> ${coins} coin(s). They now have ${newCoins} coin(s)!`
+    )
+  },
+}
