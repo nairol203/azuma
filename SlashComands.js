6df53074ba657e783719b127a5c0ad7d83e60ac1
nairol <73656754+nairol203@users.noreply.github.com>
2021-05-09 22:03:39 +0200

    Create SlashComands.js

---
+++ SlashComands.js
@@ -0,0 +1,50 @@
+module.exports.create = (name, description, options, guildId) => {
+	const app = client.api.applications(client.user.id);
+	if (guildId) {
+		app.guilds(guildId);
+	}
+	app.commands.post({
+		data: {
+			name: name,
+			description: description,
+			options: options
+		},
+	}).then(console.log(client.user.username + ' > Posted Slash-Command: ' + name));
+}
+
+module.exports.get = (guildId) => {
+	const app = client.api.applications(client.user.id);
+	if (guildId) {
+		app.guilds(guildId);
+	}
+	return app.commands.get();
+}
+
+module.exports.createApiMessage = (interaction, content) => {
+	const { data, files } = await Discord.APIMessage.create(
+		client.channels.resolve(interaction.channel_id),
+		content,
+	)
+		.resolveData()
+		.resolveFiles();
+
+	return { ...data, files };
+}
+
+module.exports.reply = (interaction, response, flags = 1) => {
+	const content = await response
+	let data = {
+		content,
+		flags
+	};
+	if (typeof content === 'object') {
+		data = await this.createApiMessage(interaction, content);
+	}
+
+	client.api.interactions(interaction.id, interaction.token).callback.post({
+		data: {
+			type: 4,
+			data,
+		},
+	});
+}
