eed780484f4659ac08f4285b4aff7fea41eaf96e
nairol <73656754+nairol203@users.noreply.github.com>
2021-06-11 22:53:05 +0200

    Update business.js

--- commands/economy/business.js
+++ commands/economy/business.js
@@ -243,29 +243,30 @@ module.exports = {
 				edit(client, interaction, embed, row);
 			}
 			else if (button.id == 'buyNext') {
+				let comp;
 				if (getBusiness.type == documents.name) {
-					company = documents;
+					comp = documents;
 				}
 				else if (getBusiness.type == weed.name) {
-					company = meth;
+					comp = meth;
 				}
 				else if (getBusiness.type == fakeMoney.name) {
-					company = fakeMoney;
+					comp = fakeMoney;
 				}
 				else if (getBusiness.type == meth.name) {
-					company = meth;
+					comp = meth;
 				};
-				await buyBusiness(guildId, userId, company.name);
-				await addCoins(guildId, userId, company.price * -1);
+				await buyBusiness(guildId, userId, comp.name);
+				await addCoins(guildId, userId, comp.price * -1);
 				const newProfit = await business.checkProfit(guildId, userId);
 				const embed = new MessageEmbed()
 					.setAuthor(`${user.username}#${user.discriminator}`, `https://cdn.discordapp.com/avatars/${userId}/${user.avatar}.webp`)
-					.setTitle(company.name)
+					.setTitle(comp.name)
 					.setDescription('Das ist die Übersicht über dein Unternehmen. Von hier aus kannst du deine Ware verkaufen und neue Upgrades für dein Business kaufen.')
 					.addFields(
 						{ name: 'Umsatz pro Verkauf', value: `${format(newProfit)} 💵` },
 						{ name: 'Lagerbestand', value: cd },
-						{ name: 'Upgrades', value: `${no} Personalupgrade\n${no} Besserer Zulieferer\n${no} ${company.nameUpgrade3}` },
+						{ name: 'Upgrades', value: `${no} Personalupgrade\n${no} Besserer Zulieferer\n${no} ${comp.nameUpgrade3}` },
 					)
 					.setFooter('Azuma | Contact @florian#0002 for help', `https://cdn.discordapp.com/avatars/${client.user.id}/${client.user.avatar}.webp`)
 					.setColor('#2f3136');
