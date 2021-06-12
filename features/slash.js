d6d05ed34f9910427f184a7927f26bf8555cf438
nairol <73656754+nairol203@users.noreply.github.com>
2021-06-12 11:02:29 +0200

    Update slash.js

--- features/slash.js
+++ features/slash.js
@@ -9,37 +9,36 @@ module.exports.send = (client, interaction, embed, components, flags = 1) => {
                 components: [
                     components,
                 ],
+                flags: flags,
             },
-            flags: flags,
         },
     });
 };
 
-module.exports.edit = (client, interaction, embed, components) => {
+module.exports.edit = (client, interaction, embed, components, content) => {
+    let data = {};
+    if (content) data.content = content;
+    if (embed) data.embeds = [embed];
+    if (components) data.components = [components];
     client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({
-        data: {
-            embeds: [
-                embed,
-            ],
-            components: [
-                components,
-            ],
-        },
+        data,
     });
 };
 
-module.exports.error = (client, interaction, content) => {
+module.exports.error = (client, interaction, content, embed, row, flags = 64) => {
+    let data = {};
+    if (content) data.content = content;
+    if (embed) data.embeds = [embed];
+    if (row) data.components = [row];
+    if (flags) data.flags = flags;
     client.api.interactions(interaction.id, interaction.token).callback.post({
         data: {
             type: 4,
-            data: {
-                content,
-                flags: 64,
-            }
+            data,
         }
     });
 };
 
 module.exports.get = (client, interaction) => {
-    return client.api.webhooks(client.user.id, interaction.token).messages('@original').get();
+    return client.api.webhooks(client.user.id, interaction.token).messages('@original').get().catch(e =>  console.log(e));
 }
