a40aefe878dbdc0cdbdaef19ab3c6d7ed2e4d91c
nairol <nairol203@web.de>
2021-07-15 23:54:31 +0200

    Create reload.js

---
+++ commands/admin/reload.js
@@ -0,0 +1,11 @@
+module.exports = {
+    callback: ({ message, args }) => {
+		const commandName = args[0].toLowerCase();
+		const command = message.client.commands.get(commandName)
+			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+		if (!command) {
+			return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		};
+    },
+};
